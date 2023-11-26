1da6b4e92a6c6ef722bba1b5f68d005c72399d51
Devagya Sharma <devagyas900@gmail.com>
2023-11-26 22:35:15 +0000

    Recipe Details New

--- src/components/recipeDetail/recipeDetail.js
+++ src/components/recipeDetail/recipeDetail.js
@@ -1,145 +1,93 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import './recipeDetail.css';
 
 const RecipeDetail = () => {
   const { id } = useParams();
   const [recipe, setRecipe] = useState(null);
   const API_KEY = process.env.REACT_APP_SPOONACULAR_API_KEY;
   const API_ENDPOINT = `https://api.spoonacular.com/recipes/${id}/information`;
-  const [successMsg, setSuccessMsg] = useState('');
+
+
   useEffect(() => {
     fetch(`${API_ENDPOINT}?apiKey=${API_KEY}`)
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         setRecipe(data);
+        console.log(data);
       })
-      .catch((error) => {
-        console.error("Error fetching recipe details:", error);
+      .catch(error => {
+        console.error('Error fetching recipe details:', error);
       });
   }, [id]);
 
-  const addToFavorites = () => {
-    const currentFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    if (currentFavorites.some(favorite => favorite.id === recipe.id)) {
-      setSuccessMsg('Recipe is already in favorites.');
-    } else {
-      const updatedFavorites = [...currentFavorites, recipe];
-      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-      setSuccessMsg('Recipe added to favorites successfully.');
-      setTimeout(() => {
-        setSuccessMsg('');
-      }, 3000); // Hide the success message after 3 seconds
-    }
-  };
-
-  if (!recipe)
-    return (
-      <div className="text-center">
-        <div className="spinner-border" role="status">
-          <span className="sr-only">Loading...</span>
-        </div>
-      </div>
-    );
+  if (!recipe) return <div>Loading...</div>;
 
   return (
-    <div className="container py-5">
-      <button onClick={addToFavorites} className="btn btn-success">
-        Add to Favorites
-      </button>
-      {successMsg && (
-        <div className="alert alert-success mt-2" role="alert">
-          {successMsg}
+    <>
+    <div className='container'>
+      <div className='row mb-2'>
+        <div className='col-3'>
+          <img className='img-fluid rounded' src={recipe.image} alt={recipe.name}/>
         </div>
-      )}
-      <h2 className="text-center mb-4">{recipe.title}</h2>
-      <div className="row justify-content-center mb-4">
-        <div className="col-lg-6">
-          <img
-            src={recipe.image}
-            alt={recipe.title}
-            className="img-fluid rounded"
-          />
+        <div className='col-9'>
+          <h1>{recipe.title}</h1>
+          <div className='row d-flex align-items-start'>
+            <div className='col'>
+              <label className='detail-label'>SERVES</label>
+              <br></br>
+              <strong>{recipe.servings} People</strong>
+            </div>
+            <div className='col'>
+              <label className='detail-label'>READY IN</label>
+              <br></br>
+              <strong>{recipe.readyInMinutes} minutes</strong>
+            </div>
+            <div className='col-4'>
+              <label className='detail-label'>PRICE PER SERVING</label>
+              <br></br>
+              <strong>${(recipe.pricePerServing/100).toFixed(2)}</strong>
+            </div>
+            <div className='col'>
+              <label className='detail-label'>HEALTH SCORE</label>
+              <br></br>
+              <strong>{recipe.healthScore}</strong>
+            </div>
+          </div>
         </div>
       </div>
-      <section className="mb-5">
-        <h3>Details:</h3>
-        <p>
-          <strong>Servings:</strong> {recipe.servings}
-        </p>
-        <p>
-          <strong>Ready In:</strong> {recipe.readyInMinutes} minutes
-        </p>
-        <p>
-          <strong>Source:</strong>{" "}
-          <a href={recipe.sourceUrl} target="_blank" rel="noopener noreferrer">
-            {recipe.sourceName}
-          </a>
-        </p>
-        <p>
-          <strong>Health Score:</strong> {recipe.healthScore}
-        </p>
-        <p>
-          <strong>Price Per Serving:</strong> $
-          {recipe.pricePerServing.toFixed(2)}
-        </p>
-      </section>
-
-      <section className="mb-5">
-        <h3>Ingredients:</h3>
-        <ul className="list-unstyled">
-          {recipe.extendedIngredients.map((ingredient) => (
-            <li key={ingredient.id} className="mb-2">
-              {ingredient.original}
-            </li>
+      <div className='row'>
+        <h2>Ingredients</h2>
+        <ul className='ms-4'>
+          {recipe.extendedIngredients.map(ingredient => (
+              <li key={ingredient.id}>
+                {ingredient.original}
+              </li>
           ))}
         </ul>
-      </section>
+      </div>
+      <div className='row'>
+        <h2>Instructions</h2>
+        {recipe.instructions ? <p dangerouslySetInnerHTML={{ __html: recipe.instructions }}></p> : <p>No instructions provided.</p>}
+      </div>
+      {recipe.winePairing.productMatches && recipe.winePairing.productMatches.length > 0 &&
+      (<div className='row'>
+        <h2>Suggested Wine Pairing</h2>
+        <div className='col-3'>
+          <img className='img-fluid rounded' src={recipe.winePairing.productMatches[0].imageUrl} alt={recipe.winePairing.productMatches[0].title}/>
+        </div>
+        <div className='col-9'>
+          <h4>{recipe.winePairing.productMatches[0].title}</h4>
+          <p><strong>${parseFloat((recipe.winePairing.productMatches[0].price).replace("$",'')).toFixed(2)} on Amazon</strong></p>
+          <p>{recipe.winePairing.productMatches[0].description}</p>
+          <a className='btn w-100 btn-primary rounded-pill' href={recipe.winePairing.productMatches[0].link} target="_blank" rel="noopener noreferrer">Buy Now on Amazon</a>
+        </div>
+      </div>)}
+      
 
-      <section className="mb-5">
-        <h3>Instructions:</h3>
-        {recipe.instructions ? (
-          <div dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
-        ) : (
-          <p>No instructions provided.</p>
-        )}
-      </section>
-
-      <section className="mb-5">
-        <h3>Wine Pairing:</h3>
-        <p>{recipe.winePairing.pairingText}</p>
-        {recipe.winePairing.productMatches &&
-          recipe.winePairing.productMatches.length > 0 && (
-            <div className="card">
-              <h4 className="card-header">Suggested Wine:</h4>
-              <img
-                src={recipe.winePairing.productMatches[0].imageUrl}
-                alt={recipe.winePairing.productMatches[0].title}
-                className="card-img-top"
-              />
-              <div className="card-body">
-                <h5 className="card-title">
-                  {recipe.winePairing.productMatches[0].title}
-                </h5>
-                <p className="card-text">
-                  {recipe.winePairing.productMatches[0].description}
-                </p>
-                <p className="card-text">
-                  Price: {recipe.winePairing.productMatches[0].price}
-                </p>
-                <a
-                  href={recipe.winePairing.productMatches[0].link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-primary"
-                >
-                  Buy Now
-                </a>
-              </div>
-            </div>
-          )}
-      </section>
     </div>
+    </>
   );
-};
+}
 
 export default RecipeDetail;
